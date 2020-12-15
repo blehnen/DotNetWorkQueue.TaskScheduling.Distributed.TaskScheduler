ad70a19a6ec2e0ecfb7c417014f5d477bf6ec0ff
Brian Lehnen <brian.lehnen74@gmail.com>
2020-12-15 17:22:05 -0600

    1) Target latest version of DotNetWorkQueue

--- Source/ITaskSchedulerJobCountSync.cs
+++ Source/ITaskSchedulerJobCountSync.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------
 //This file is part of DotNetWorkQueue
-//Copyright © 2017 Brian Lehnen
+//Copyright © 2017-2020 Brian Lehnen
 //
 //This library is free software; you can redistribute it and/or
 //modify it under the terms of the GNU Lesser General Public
